a0c43af7a9eeda9e4c875d656232be724cfddb1f
Tom Hinton <tom.hinton@cse.org.uk>
2023-04-14 23:40:01 +0100

    timers

--- src/spirit.clj
+++ src/spirit.clj
@@ -1,11 +1,14 @@
 (ns spirit
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.tools.logging :as log]
             [instaparse.core :as insta]
             [instaparse.combinators :as c]
             [clojure.string :as string]
             [clj-fuzzy.metrics :refer [levenshtein]]
-            [hato.client :as http])
+            [hato.client :as http]
+            [chime.core :as chime])
+  (:import [java.time Instant])
   (:gen-class))
 
 (def metagrammar (insta/parser (io/resource "metagrammar.g")))
@@ -29,12 +32,12 @@
             (->> reason
                  (filter (comp #{:string} :tag))
                  (map :expecting)
-                 (map (fn [e] [(levenshtein w e) e]))
-                 (filter #(< (first %) (/ (count (second %)) 2)))
+                 (map (fn find-l [e] [(levenshtein w e) e]))
+                 (filter (fn filter-l [%] (< (first %) (/ (count (second %)) 2))))
                  (sort))
             ]
         (or (first (keep
-                    (fn [[ld e]]
+                    (fn keep-parses [[ld e]]
                       (let [s (str up-to " " e " " rest-s)]
                         (let [r (parse-with-fuzz g s)]
                           (when-not (insta/failure? r)
@@ -45,9 +48,8 @@
 
 (defn load-grammar [filename]
   (let [g (metagrammar (slurp (io/as-file filename)))
-        _ (when (insta/failure? g)
-            (println g))
-        wrap-strings #(map (fn [i]
+        _ (when (insta/failure? g) (log/error "Parsing" filename g))
+        wrap-strings #(map (fn wrap-strings [i]
                              (if (string? i)
                                (c/cat (c/hide (c/regexp #"\s*"))
                                       (c/string i)
@@ -59,7 +61,7 @@
         extra (atom {})
         ctr (atom 0)
         gr (insta/transform
-            {:G (fn [& items] (vec items))
+            {:G (fn transform-g [& items] (vec items))
              :command (fn command
                         ([label args sentence]
                          (let [label0 (keyword label)
@@ -89,7 +91,7 @@
             )
         gr (into {} gr)
         {:keys [captures commands command-args]} @extra
-        gr (reduce (fn [a [l l0 r]] (assoc a l r)) gr captures)
+        gr (reduce (fn add-captures [a [l l0 r]] (assoc a l r)) gr captures)
 
         parser
         (insta/parser (merge
@@ -101,19 +103,20 @@
     (fn parse-line [line]
       (let [result (insta/transform
                     (merge
-                     (into {} (for [[l l0 _] captures] [l (fn [v] [l0 v])]))
+                     (into {} (for [[l l0 _] captures] [l (fn captures [v] [l0 v])]))
                      {:integer read-string
                       :hours (fn [h & [m]] (+ (or m 0) 60 (* 60 h)))
                       :minutes (fn [m] (* 60 m))
                       :seconds identity
                       :time identity
-                      :root (fn [hs]
+                      :root (fn root [hs]
                               {:command (first hs)
                                :args (into {} (remove string? (rest hs)))})})
 
                     (parse-with-fuzz parser line))]
 
-        (-> (update result :args #(merge (command-args (:command result)) %))
+        (-> (update result :args (fn merge-args [%]
+                                   (merge (command-args (:command result)) %)))
             (assoc :command (commands (:command result))))
         ))))
 
@@ -121,26 +124,27 @@
                          :ha/token ""
                          :ha/args {}})
 (defn ha-call [url body]
-  (-> (http/post
-       (str (:ha/url *config*) url)
+  (let [resp (http/post
+              (str (:ha/url *config*) url)
 
-       {:accept :json
-        :as :json
-        :content-type :json
-        :form-params body
-        :headers {"Authorization" (format "Bearer %s"
-                                          (:ha/token *config*))}})
-      
-      (:body)))
+              {:accept :json
+               :as :json
+               :content-type :json
+               :form-params body
+               :headers {"Authorization" (format "Bearer %s"
+                                                 (:ha/token *config*))}})]
+    (log/info "CALL" url body "=>" (:status resp) (:body resp))
+    resp))
 
 (defn lms [player command & args]
   (loop [n 0]
     (let [resp
           (-> (ha-call "/services/squeezebox/call_query"
                        (cond-> {:entity_id player :command (name command)}
-                         (seq args) (assoc :parameters (vec (map name args))))))]
+                         (seq args) (assoc :parameters (vec (map name args)))))
+              (:body))]
       (if (and (= [] resp) (< n 5))
-        (do (Thread/sleep 500) (recur (inc n)))
+        (do (Thread/sleep 750) (recur (inc n)))
         (-> resp
             (get 0)
             (:attributes)
@@ -150,9 +154,11 @@
   (let [mode (-> (lms player :mode :?) :_mode)
         time (-> (lms player :time :?) :_time)]
     (let [[[url title] & urls] urls]
-      (lms player :playlist :preview (str "url:" url) (str "title:" title))
+      (lms player :playlist :preview
+           (str "url:" url) (str "title:" title))
       (doseq [[url title] urls]
-        (lms player :playlist :add (str "url:" url) (str "title:" title))))
+        (lms player :playlist :add
+             (str "url:" url) (str "title:" title))))
     
     (loop []
       (Thread/sleep 1500)
@@ -161,29 +167,108 @@
     (lms player :playlist :preview "cmd:stop")
     (when (= mode "play")
       (lms player :play "1")
-      (Thread/sleep 750)
+      (Thread/sleep 1000)
+      (log/info "seek back to" time)
+      (lms player :time (str time))
       (lms player :time (str time)))))
 
-(def ^:dynamic *media-player* "media_player.top_room")
+(defn sound-url [sound]
+  (case sound
+    :command-error (str (:sound-prefix *config*) "/error.mp3")
+    :success (str (:sound-prefix *config*) "/"
+                  (first (shuffle ["chirp1" "chirp2"])) ".mp3")
+    :chime (str (:sound-prefix *config*) "/chirp1.mp3")))
+
+(defn tts-url [message]
+  (:url (:body (ha-call
+                "/tts_get_url"
+                {:platform "google_translate"
+                 :message message}))))
 
 (defn speak [message]
   (play-urls
-   *media-player*
-   [[(:url (ha-call
-            "/tts_get_url"
-            {:platform "google_translate"
-             :message message}))
-     message]]))
+   (:ha/media-player *config*)
+   [[(tts-url message) message]]))
 
-(defmulti handle-command (fn [config c] (keyword (namespace (:command c)))))
+(defmulti handle-command
+  (fn [c] (keyword (or (namespace (:command c))
+                       ::no-namespace))))
 
-(defmethod handle-command :ha [config {:keys [command args]}]
+(defmethod handle-command :ha [{:keys [command args]}]
   (let [service (name command)]
-    (ha-call (str "/services/" (string/replace service #"\." "/"))
-             (merge (:ha/args *config*) args))))
+    (let [status
+          (:status
+           (ha-call (str "/services/" (string/replace service #"\." "/"))
+                    (merge (:ha/args *config*) args)))
+          player (:ha/media-player *config*)]
+      (if (= 200 status)
+        (when-not (= "play" (:_mode (lms player :mode :?)))
+          (play-urls player [[(sound-url :success) "Success"]]))
+        (play-urls player [[(sound-url :command-error) "Command error"]])))))
 
-(defmethod handle-command :default [config c]
-  (println "Unknown command" c))
+(def timers (atom {}))
+
+
+(defn time-difference [now then]
+  (let [delta (abs (.getSeconds (java.time.Duration/between now then)))
+        minutes (int (/ delta 60))
+        seconds (mod delta 60)
+        hours (int (/ minutes 60))
+        minutes (mod minutes 60)
+        ]
+    (cond
+      (and (= 0 seconds minutes hours)) "now"
+
+      :else
+      (cond-> ""
+        (= 1 hours)   (str "1 hour ")
+        (> hours 1)   (str hours " hours ")
+        (= 1 minutes) (str "1 minute ")
+        (> minutes 1) (str minutes " minutes ")
+        (= 1 seconds) (str "1 second")
+        (> seconds 1) (str seconds " seconds")))))
+
+
+(defmethod handle-command :timer [{:keys [command args]}]
+  (let [{:keys [time n]} args]
+    (case command
+      :timer/set    (let [now (Instant/now)
+                          then (.plusSeconds now time)
+                          timer-name (atom nil)
+                          config *config*]
+                      (swap! timers
+                             (fn set-timer [cur-timers]
+                               (let [next-id (first (remove (set (keys cur-timers)) (rest (range))))]
+                                 (reset! timer-name next-id)
+                                 (assoc cur-timers next-id
+                                        [then (chime/chime-at
+                                               [then]
+                                               (fn [_]
+                                                 (log/info "timer" next-id "finished")
+                                                 (swap! timers dissoc next-id)
+                                                 (binding [*config* config]
+                                                   (play-urls
+                                                    (:ha/media-player config)
+                                                    [[(tts-url (format "Timer %s finished" next-id))
+                                                      "Timer finished"]
+                                                     [(sound-url :chime) "Chime"]]))))]))))
+                      (speak (format "Timer %s started for %s" @timer-name (time-difference now then))))
+      
+      :timer/cancel (do (.close (get @timers n))
+                        (swap! timers dissoc n)
+                        (speak (format "Timer %s cancelled" n)))
+      :timer/list   (let [timers @timers
+                          now (Instant/now)
+                          message (string/join
+                                   ", "
+                                   (for [[timer-id [time _]] timers]
+                                     (format "timer %s in %s"
+                                             timer-id
+                                             (time-difference now time))))]
+                      (speak message)))))
+
+(defmethod handle-command :default [c]
+  (log/error "Unknown command" c))
 
 (defn run [{:keys [grammar config]}]
   (binding [*config* (edn/read (java.io.PushbackReader.
@@ -192,13 +277,16 @@
       (loop []
         (let [line (read-line)]
           (when (string/starts-with? line "COM ")
-            (let [result (parser (subs line 4))]
+            (let [line (subs line 4)
+                  result (parser line)]
               (if (insta/failure? result)
-                (println "nope" line result)
-                (handle-command result)))))
-        (recur)))))
-
+                (do (log/error
+                     "Unable to parse command"
+                     line result)
+                    (speak (str "I don't understand " line)))
+                (do (log/info "Parsed" line "to" result)
+                    (handle-command result)))))
+          (when-not (= line "quit") (recur)))))))
 
 (defn -main [grammar config]
   (run {:grammar grammar :config config}))
-
